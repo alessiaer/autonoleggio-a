88d2d4f1aba1f99837fcbe4098cead79a559361b
alessiaer <ercolino.alessia@gmail.com>
2023-01-24 17:32:23 +0100

    ricerca
    elaborazione programma, devo capire effettivamente se i controlli non vengono effettuati per via della funzione vett o per errori nella sintassi
    
    Co-Authored-By: alesser <31736250+alesser@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -12,98 +12,64 @@ struct s_veicolo
     string marca;
     string modello;
     string colore;
-    char giorni[7];
+    string giorni[7];
 }veicolo[M];
 
 void visualizzazione()
 {
-  ofstream fout ("auto.csv");
+  ifstream fin ("auto.csv");
 
-  cout<<"Categoria, Marca, Modello, Colore, Lunedi, Martedi, Mercoledi, Giovedi, Venerdi, Sabato, Domenica"<<endl;
-  fout<<"Categoria, Marca, Modello, Colore, Lunedì, Martedì, Mercoledì, Giovedì, Venerdì, Sabato, Domenica"<<endl;
+  string app;
+  while(!fin.eof())
+  {
+       getline(fin,app);
+       cout<<'\t'<<app<<endl;
+  }
 
-  for(int i=0; i<M; i++)
-   {
-     cout<<veicolo[i].categoria<<',';
-     fout<< veicolo[i].categoria<<',';
-
-     cout<< veicolo[i].marca<<',';
-     fout<< veicolo[i].marca<<',';
-
-     cout<< veicolo[i].modello<<',';
-     fout<< veicolo[i].modello<<',';
-
-     cout<< veicolo[i].colore<<endl;
-     fout<< veicolo[i].colore<<endl;
-   }
-  fout.close();
+  fin.close();
 }
 
-void inserimento()
+void vett()
 {
-    fstream fout ("auto.csv");
-    cin.ignore();
+    ifstream fin ("auto.csv");
+    int j=0;
+    while( !fin.eof())
+    {
+        getline(fin, veicolo[j].categoria,',');
+        getline(fin, veicolo[j].marca,',');
+        getline(fin, veicolo[j].modello,',');
+        getline(fin, veicolo[j].colore,',');
+        for(int i=0; i<6; i++)
+           getline(fin, veicolo[j].giorni[i],',');
+        getline(fin, veicolo[j].giorni[6]);
+        j++;
+    }
 
-     //categoria
-     veicolo[0].categoria="utilitaria";
-     veicolo[1].categoria="utilitaria";
-     veicolo[2].categoria="lusso";
-     veicolo[3].categoria="sportiva";
-     veicolo[4].categoria="furgone";
-     veicolo[5].categoria="lusso";
-     veicolo[6].categoria="utilitaria";
-
-     //marca
-     veicolo[0].marca="FIAT";
-     veicolo[1].marca="KIA";
-     veicolo[2].marca="Mercedes";
-     veicolo[3].marca="Lamborghini";
-     veicolo[4].marca="Ford";
-     veicolo[5].marca="BMW";
-     veicolo[6].marca="Peugeot";
-
-     //modello
-     veicolo[0].modello="Panda";
-     veicolo[1].modello="Pikanto";
-     veicolo[2].modello="Classe 5";
-     veicolo[3].modello="Huracan";
-     veicolo[4].modello="Transit";
-     veicolo[5].modello="Serie 5";
-     veicolo[6].modello="Peugeot";
-
-     //colore
-     veicolo[0].colore="rosso";
-     veicolo[1].colore="bronzo";
-     veicolo[2].colore="nero";
-     veicolo[3].colore="giallo";
-     veicolo[4].colore="bianco";
-     veicolo[5].colore="metallizzato";
-     veicolo[6].colore="verde";
-
-     //giorni
-    /* veicolo[0].giorni{'L','L','L','A','A','A','A'};
-       veicolo[1].giorni[]={'A','A','A','A','L','A','A'};
-       veicolo[2].giorni[]={'L','L','L','L','L','A','A'};
-       veicolo[3].giorni[]={'L','L','L','L','L','L','L'};
-       veicolo[4].giorni[]={'A','A','A','A','A','L','L'};
-       veicolo[5].giorni[]={'L','L','L','L','L','A','A'};
-       veicolo[6].giorni[]={'L','A','L','L','L','A','L'};
-
-*/
-
-      visualizzazione();
-
-
-
-fout.close();
+     fin.close();
 }
+
+int conta()
+{
+    int y=0;
+    string sos;
+    fstream fin (auto_csv, ios::in);
+    while(getline(fin, sos))
+    {
+        y++;
+         getline(fin,sos);
+    }
+
+    fin.close();
+    return y-1;
+}
+
 
 void selezione()
 {
     ifstream fin(auto_csv);
 
     string cat, gio[7], app;
-    int t=0, g,s ;
+    int  t=0, g,s ;
 
     cout<<"scegli la categoria a cui sei interessato: " ;
     cin>>cat;
@@ -117,60 +83,61 @@ void selezione()
         vet[g]=s;
         g--;}
     while(g!=0);
+    vett();
 
-  cout<<"le auto disponibili sono: \n";
-
-   while(!fin.eof())
+    while(!fin.eof())
     {
-        for(int h=0; h<7; h++)
+        if(cat==veicolo[t].categoria)
         {
+            cout<<"ale non sei cosi tanto una fallita";
+            for(int h=0; h<7; h++)
+           {
+            if(vet[t]==h) //&& veicolo[t].giorni[h]=="L")
+            {
+                //stampa la riga
+                cout<<"ale non sei cosi tanto una fallita";
+            }
+        }
+        }
 
-            if(vet[t]== h && veicolo[t].giorni[h]== 'L')
-                {
 
-                    getline(fin,app);
-                    cout<<'\t'<<app<<endl; //agg var per fare 1)..
-                    cout<<"\t_______________________________________\n";
-                }
-         }
-       t++;
+
+
+        t++;
     }
 
+
+
+
+
+
+
+
+
+
+  cout<<"le auto disponibili sono: \n";
     int p;
     cout<<"Quale vuoi prenotare? ";
     cin>>p;
-    //controllare tipo menu? <aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 
     fin.close();
 }
 
-
-
-int conta()
-{
-    int y=0;
-    string sos;
-    fstream fin (auto_csv, ios::in);
-    while(getline(fin, sos))
-          y++;
-    fin.close();
-    return y;
-}
 
 
 void modifica()
 {
+    int p;
     cout<<"Quale vuoi prenotare?";
     cin>>p;
 
-    getline(cin, veicolo.giorno)
-    int con=conta();
+   // getline(cin, veicolo.giorno)
+   // int con=conta();
     ofstream fout( auto_csv);
 
     fout.close();
     visualizzazione();
 }
-
 
 
 int main()
@@ -182,7 +149,7 @@ int main()
 
     switch(s)
     {
-        case 1: inserimento();
+        case 1: visualizzazione();
                 break;
         case 2: selezione();
                 break;
